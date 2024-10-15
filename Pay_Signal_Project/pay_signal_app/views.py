c25341611d96873727dfe7cc58456e12a2e8a6e1
Ethan-Software-Developer <ethansevenster5@gmail.com>
2024-10-15 03:16:40 -0700

    changed something

---
+++ Pay_Signal_Project/pay_signal_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
+from django.contrib import messages
+
+# Create your views here.
+
+<<<<<<< HEAD
+=======
+def home(request):
+    return render(request, "home.html")
+
+
+>>>>>>> eafcb10 (added home template and checked database)
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # Automatically log the user in after registration
+            messages.success(request, 'Registration successful!')
+            return redirect("home") # Redirect to home page
+    else:
+        form = UserRegistrationForm()
+        
+        
+    return render(request, "register.html", {"form": form})
